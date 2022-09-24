12da1dbb1edddcac571d432890a59ef9d1eea68c
abnazhor <jorbar982@outlook.com>
2022-09-24 13:02:24 +0200

    Add logger to export

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import loadRoutesFn from "./loadRoutes.js";
+import { info, success, warn, error } from "./utils/logger.js";
 
 export const loadRoutes = loadRoutesFn;
+export const logger = {
+  error,
+  info,
+  success,
+  warn,
+};
